dcfddfbba4e07dca378756978dfa940c2bf7399e
Luis Masuelli <luisfmasuelli@gmail.com>
2025-11-09 15:37:33 -0500

    Adding the method to build the headers.

---
+++ python/y402/facilitator_client/errors.py
@@ -0,0 +1,7 @@
+from y402.core.types.errors import BaseError
+
+
+class HeadersBuildingFacilitatorError(BaseError):
+    """
+    Raised when there was an error creating the facilitator headers.
+    """
---
+++ python/y402/facilitator_client/utils.py
@@ -0,0 +1,29 @@
+import inspect
+import asyncio
+from typing import Literal
+from y402.core.types.facilitator import FacilitatorHeaders
+from y402.facilitator_client.errors import HeadersBuildingFacilitatorError
+
+
+def make_headers(headers: FacilitatorHeaders, endpoint: Literal["settle", "verify"]) -> dict:
+    """
+    Makes the headers out of a spec. This method is synchronous.
+
+    Args:
+        headers: The headers spec for the facilitator.
+        endpoint: 'settle' or 'verify'.
+    Returns:
+        A dictionary with the headers.
+    """
+
+    stuff = headers.get(endpoint, {})
+    if isinstance(stuff, dict):
+        return stuff
+    else:  # callable
+        try:
+            obj = stuff()
+            if inspect.isawaitable(stuff):
+                return asyncio.run(obj)
+            return obj
+        except Exception as e:
+            raise HeadersBuildingFacilitatorError(e)
