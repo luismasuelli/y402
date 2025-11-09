c8e157075ea8c0e1bd1601a15646a6bd3703eed1
Luis Masuelli <luisfmasuelli@gmail.com>
2025-11-09 14:32:14 -0500

    A slight reformat.

--- python/y402/core/types/errors.py
+++ python/y402/core/types/errors.py
@@ -3,10 +3,12 @@ class BaseError(Exception):
     Base error, for all the error subclasses, of this library.
     """
 
+
 class MisconfigurationError(BaseError):
     """
     Raised when a configuration error occurs.
     """
+
 
 class ConditionalDependencyError(BaseError):
     """
