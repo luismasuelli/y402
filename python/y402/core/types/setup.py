9b5953b44120b137dae97263ec64d5855fc3c7a3
Luis Masuelli <luisfmasuelli@gmail.com>
2025-11-09 21:50:41 -0500

    Adding the setup tokens.

--- python/y402/core/types/setup.py
+++ python/y402/core/types/setup.py
@@ -2,41 +2,109 @@ from y402.core.types.errors import MisconfigurationError
 
 
 KNOWN_DATA = {
-    "abstract": {
-        "chain_id": 2741
-    },
-    "abstract-testnet": {
-        "chain_id": 11124
-    },
     "base": {
-        "chain_id": 8453
+        "chain_id": 8453,
+        "tokens": [{
+            "code": "usdc",
+            "address": "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+            "name": "USD Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }, {
+            "code": "eurc",
+            "address": "0x60a3E35Cc302bFA44Cb288Bc5a4F316Fdb1adb42",
+            "name": "EUR Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }]
     },
     "base-sepolia": {
-        "chain_id": 84532
+        "chain_id": 84532,
+        "tokens": [{
+            "code": "usdc",
+            "address": "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
+            "name": "USD Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }, {
+            "code": "eurc",
+            "address": "0x808456652fdb597867f38412077A9182bf77359F",
+            "name": "EUR Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }]
     },
     "avalanche": {
-        "chain_id": 43114
+        "chain_id": 43114,
+        "tokens": [{
+            "code": "usdc",
+            "address": "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
+            "name": "USD Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }, {
+            "code": "eurc",
+            "address": "0xc891eb4cbdeff6e073e859e987815ed1505c2acd",
+            "name": "EUR Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }]
     },
     "avalanche-fuji": {
-        "chain_id": 43113
+        "chain_id": 43113,
+        "tokens": [{
+            "code": "usdc",
+            "address": "0x5425890298aed601595a70AB815c96711a31Bc65",
+            "name": "USD Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }, {
+            "code": "eurc",
+            "address": "0x5E44db7996c682E92a960b65AC713a54AD815c6B",
+            "name": "EUR Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }]
     },
     "sei": {
-        "chain_id": 1329
+        "chain_id": 1329,
+        "tokens": [{
+            "code": "usdc",
+            "address": "0xe15fC38F6D8c56aF07bbCBe3BAf5708A2Bf42392",
+            "name": "USD Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }]
     },
     "sei-testnet": {
-        "chain_id": 1328
+        "chain_id": 1328,
+        "tokens": [{
+            "code": "usdc",
+            "address": "0x4fCF1784B31630811181f670Aea7A7bEF803eaED",
+            "name": "USD Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }]
     },
     "polygon": {
-        "chain_id": 137
+        "chain_id": 137,
+        "tokens": [{
+            "code": "usdc",
+            "address": "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+            "name": "USD Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }]
     },
     "polygon-amoy": {
-        "chain_id": 80002
-    },
-    "iotex": {
-        "chain_id": 4689
-    },
-    "peaq": {
-        "chain_id": 3338
+        "chain_id": 80002,
+        "tokens": [{
+            "code": "usdc",
+            "address": "0x41E94Eb019C0762f9Bfcf9Fb1E58725BfB0e7582",
+            "name": "USD Coin",
+            "decimals": 6,
+            "eip712Version": "2"
+        }]
     }
 }
 
@@ -78,9 +146,14 @@ class Y402Setup:
             "tokens": {}
         }
 
-    def add_token(self, network: str, code: str, name: str,
-                  address: str, version: str,
+    def add_token(self, network: str, code: str,
+                  # The name.
+                  name: str = "",
+                  # The address and the version.
+                  address: str = "", version: str = "",
+                  # The decimals and the symbol.
                   decimals: int = 6, symbol: str = "",
+                  # Whether it's default or not.
                   default_for_symbol: bool = False):
         """
         Adds a token contract to a specific place.
@@ -113,4 +186,7 @@ class Y402Setup:
             raise MisconfigurationError(f"Use a valid code for the token")
         if code in self._networks[network]["tokens"]:
             raise MisconfigurationError(f"This token is already set up in network {network}: {code}")
-        # TODO continue implementing this.from
+
+        name = name.strip() or code.upper()
+        if not address or not version:
+            pass
