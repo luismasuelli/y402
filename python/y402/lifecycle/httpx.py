779c7013ae1d1d5c98ef6cb834ced3e5612e9cd5
Luis Masuelli <luisfmasuelli@gmail.com>
2025-11-09 22:58:55 -0500

    Finishing the process_payment async httpx implementation.

--- python/y402/lifecycle/httpx.py
+++ python/y402/lifecycle/httpx.py
@@ -1,24 +1,38 @@
 import datetime
+from typing import List
 from uuid import uuid4
 from y402.core.types.client import PaymentPayload
 from y402.core.types.facilitator import VerifyRequest, X402_VERSION, FacilitatorConfig, SettleRequest
-from y402.core.types.payment import SettledPayment, PaymentIdentity, PaymentDetails
 from y402.core.types.requirements import PaymentRequirements
+from y402.core.types.setup import Y402Setup
 from y402.core.types.storage import StorageManager
 from y402.facilitator_client.httpx import FacilitatorClient
+from y402.lifecycle.utils import create_settled_payment
 from y402.triggers.httpx import send_payment
 
 
-async def process_payment(payment: PaymentPayload, matched_requirements: PaymentRequirements,
-                          facilitator_config: FacilitatorConfig, storage_manager: StorageManager,
-                          webhook_url: str, api_key: str = None,
-                          request_timeout: int = 15, webhook_timeout: int = 15):
+async def process_payment(
+    # The identity of the payment.
+    resource: str, tags: List[str], reference: str,
+    # User payment selection.
+    payment: PaymentPayload, matched_requirements: PaymentRequirements,
+    # External components.
+    setup: Y402Setup, facilitator_config: FacilitatorConfig, storage_manager: StorageManager,
+    # Webhook-related data.
+    webhook_url: str, api_key: str = None,
+    # Tunings
+    request_timeout: int = 15, webhook_timeout: int = 15
+):
     """
     Processes a given payment.
 
     Args:
+        resource: The (PUBLIC) resource URL.
+        tags: The tags that apply.
+        reference: The reference that applies.
         payment: The user-submitted payment.
         matched_requirements: The matched requirements.
+        setup: An existing Y402 setup.
         facilitator_config: The facilitator config to use to create a client.
         storage_manager: The storage manager for payments.
         webhook_url: The URL of the webhook to ping.
@@ -51,24 +65,15 @@ async def process_payment(payment: PaymentPayload, matched_requirements: Payment
             timeout=request_timeout
         ))
         await storage_manager.commit(payment_id)
-        # TODO continue these all fields.
-        settled_payment = SettledPayment(
-            id_=payment_id,
-            identity=PaymentIdentity(
-                resource=...,
-                tags=[...],
-                reference=...
-            ),
-            details=PaymentDetails(
-                payer=...,
-                chain_id=...,
-                token=...,
-                value=...,
-                code=...,
-                name=...,
-                price_label=...,
-            ),
-            settled_on=datetime.datetime.now(tz=datetime.timezone.utc)
+        payer = payment.payload.authorization.from_
+        network = payment.network
+        token = matched_requirements.asset
+        value = payment.payload.authorization.value
+        chain_id, code, name, price_label = setup.get_token_data(network, token, value)
+        settled_payment = create_settled_payment(
+            payment_id, resource, tags, reference,
+            payer, chain_id, token, value,
+            code, name, price_label
         )
         await send_payment(webhook_url, settled_payment, api_key, webhook_timeout)
     except:
