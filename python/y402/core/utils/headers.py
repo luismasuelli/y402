790bf04190df5335a09ad4dd6e26c1352c1952dc
Luis Masuelli <luisfmasuelli@gmail.com>
2025-11-15 00:02:58 -0500

    Changing the header validator return value and a check.

--- python/y402/core/utils/headers.py
+++ python/y402/core/utils/headers.py
@@ -44,7 +44,7 @@ def validate_payment_asset(
     payment_payload: PaymentPayload,
     payment_asset_header: str,
     merged_setup: Y402Setup
-) -> Tuple[str, str]:
+) -> Tuple[str, bool]:
     """
     Validates whether the asset in the payment asset header is valid or not.
 
@@ -54,17 +54,18 @@ def validate_payment_asset(
         payment_asset_header: The contents of the payment asset header.
         merged_setup: The current merged setup.
     Returns:
-        A tuple (code, "ok") or ("", "no_match").
+        A tuple (code, True) or ("", False).
     """
 
     token_codes = merged_setup.list_tokens(network)
     chain_id = merged_setup.get_chain_id(network)
+    payment_asset_header = payment_asset_header.lower()
 
     for code in token_codes:
         name, _, address, version, _ = merged_setup.get_token_metadata(network, code)
         if check_signature(name, version, chain_id, address,
                            payment_payload.payload.authorization,
                            payment_payload.payload.signature) and \
-                (not payment_asset_header or payment_asset_header == address):
-            return code, "ok"
-    return "", "no_match"
+                (not payment_asset_header or payment_asset_header == address.lower()):
+            return code, True
+    return "", False
