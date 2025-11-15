ae36c842047ea327cc76dae09c2481f2b1947104
Luis Masuelli <luisfmasuelli@gmail.com>
2025-11-14 21:21:52 -0500

    Modifying the registry to add more fields.

--- python/y402/core/types/registry.py
+++ python/y402/core/types/registry.py
@@ -1,8 +1,9 @@
+from typing import Optional
 from .endpoint_settings import X402_ENDPOINT_SETTINGS
 from .setup import Y402Setup
 
 
-class ExtendedSupportRegistry:
+class FinalEndpointSetupRegistry:
     """
     This is a registry of endpoints, under a single middleware,
     and the support for their networks and tokens.
@@ -12,7 +13,7 @@ class ExtendedSupportRegistry:
     is generated and retrieved.
     """
 
-    def __init__(self, middleware_custom_setup: Y402Setup):
+    def __init__(self, middleware_custom_setup: Optional[Y402Setup]):
         self._full_data_by_endpoint = {}
         self._middleware_custom_setup = middleware_custom_setup
 
@@ -20,5 +21,9 @@ class ExtendedSupportRegistry:
         if item not in self._full_data_by_endpoint:
             endpoint_settings = getattr(item, X402_ENDPOINT_SETTINGS, None)
             endpoint_custom_setup = endpoint_settings and endpoint_settings.custom_setup
-            self._full_data_by_endpoint[item] = self._middleware_custom_setup | endpoint_custom_setup
+            if self._middleware_custom_setup and endpoint_custom_setup:
+                setup = self._middleware_custom_setup | endpoint_custom_setup
+            else:
+                setup = self._middleware_custom_setup or endpoint_custom_setup or Y402Setup()
+            self._full_data_by_endpoint[item] = setup
         return self._full_data_by_endpoint[item]
