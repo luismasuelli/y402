9eebb644a86ab27b350741eaa29c76d7b3d3e11d
Luis Masuelli <luisfmasuelli@gmail.com>
2025-11-14 23:48:44 -0500

    Adding a function named validate_payment_asset. It returns the token code that matches the signature.

--- python/y402/core/utils/headers.py
+++ python/y402/core/utils/headers.py
@@ -1,7 +1,9 @@
 import base64
 import json
-from typing import Dict, Any
+from typing import Dict, Any, Tuple
 from y402.core.types.client import PaymentPayload
+from y402.core.types.setup import Y402Setup
+from .signature import check_signature
 
 
 def is_browser_request(headers: Dict[str, Any]) -> bool:
@@ -9,10 +11,9 @@ def is_browser_request(headers: Dict[str, Any]) -> bool:
     Determine if request is from a browser vs API client.
 
     Args:
-        headers: Dictionary of request headers (case-insensitive keys)
-
+        headers: Dictionary of request headers (case-insensitive keys).
     Returns:
-        True if request appears to be from a browser, False otherwise
+        True if request appears to be from a browser, False otherwise.
     """
 
     headers_lower = {k.lower(): v for k, v in headers.items()}
@@ -36,3 +37,34 @@ def decode_payment_header(payment_header: str) -> PaymentPayload:
     """
 
     return PaymentPayload(**json.loads(base64.b64decode(payment_header).decode("utf-8")))
+
+
+def validate_payment_asset(
+    network: str,
+    payment_payload: PaymentPayload,
+    payment_asset_header: str,
+    merged_setup: Y402Setup
+) -> Tuple[str, str]:
+    """
+    Validates whether the asset in the payment asset header is valid or not.
+
+    Args:
+        network: The involved chosen network.
+        payment_payload: The provided payment payload.
+        payment_asset_header: The contents of the payment asset header.
+        merged_setup: The current merged setup.
+    Returns:
+        A tuple (code, "ok") or ("", "no_match").
+    """
+
+    token_codes = merged_setup.list_tokens(network)
+    chain_id = merged_setup.get_chain_id(network)
+
+    for code in token_codes:
+        name, _, address, version, _ = merged_setup.get_token_metadata(network, code)
+        if check_signature(name, version, chain_id, address,
+                           payment_payload.payload.authorization,
+                           payment_payload.payload.signature) and \
+                (not payment_asset_header or payment_asset_header == address):
+            return code, "ok"
+    return "", "no_match"
