4886fb6ce880bb2c5b549acd40178362eb0193bf
Luis Masuelli <luisfmasuelli@gmail.com>
2025-11-16 20:53:49 -0500

    Adding the dynamic response for flask middleware.

--- python/y402/flask/response_presets.py
+++ python/y402/flask/response_presets.py
@@ -8,16 +8,17 @@ from y402.core.utils.headers import is_browser_request
 from y402.core.utils.html import get_paywall_html
 
 
-def x402_response(
-    error: str,
+def response(
+    status_code: int, error: str,
     custom_paywall_html: Optional[str],
     paywall_config: Optional[PaywallConfig],
     payment_requirements: List[PaymentRequirements],
 ) -> Response:
     """
-    Creates a 402 response with payment requirements.
+    Creates a dynamic response with payment requirements.
 
     Args:
+        status_code: The status code of the response.
         error: The message of the concrete error that occurred.
         custom_paywall_html: The paywall HTML to render for HTML requests.
         paywall_config: The configuration for the paywall.
@@ -27,7 +28,6 @@ def x402_response(
     """
 
     request_headers = dict(request.headers)
-    status_code = 402
 
     if is_browser_request(request_headers):
         html_content = custom_paywall_html or get_paywall_html(
@@ -50,3 +50,26 @@ def x402_response(
         resp = make_response(jsonify(response_data), status_code)
         resp.headers.update(headers)
         return resp
+
+
+def x402_response(
+    error: str,
+    custom_paywall_html: Optional[str],
+    paywall_config: Optional[PaywallConfig],
+    payment_requirements: List[PaymentRequirements],
+) -> Response:
+    """
+    Creates a 402 response with payment requirements.
+
+    Args:
+        error: The message of the concrete error that occurred.
+        custom_paywall_html: The paywall HTML to render for HTML requests.
+        paywall_config: The configuration for the paywall.
+        payment_requirements: The alternate payment requirements.
+    Returns:
+        A Response object.
+    """
+    return response(
+        402, error, custom_paywall_html,
+        paywall_config, payment_requirements
+    )
