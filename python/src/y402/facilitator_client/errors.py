717ab2f41ec564ea9ebedaf7fde97f1e092a0000
Luis Masuelli <luisfmasuelli@gmail.com>
2025-11-25 20:49:02 -0500

    Adding an intermediate facilitator error.

--- python/src/y402/facilitator_client/errors.py
+++ python/src/y402/facilitator_client/errors.py
@@ -1,43 +1,49 @@
 from ..core.types.errors import BaseError
 
 
-class HeadersBuildingFacilitatorError(BaseError):
+class BaseFacilitatorError(BaseError):
+    """
+    Raised when there was an error related to a facilitator.
+    """
+
+
+class HeadersBuildingFacilitatorError(BaseFacilitatorError):
     """
     Raised when there was an error creating the facilitator headers.
     """
 
 
-class VerifyBadResponse(BaseError):
+class VerifyBadResponse(BaseFacilitatorError):
     """
     Raised when the verification returned a non-2xx error.
     """
 
 
-class VerifyFacilitatorInvalidError(BaseError):
+class VerifyFacilitatorInvalidError(BaseFacilitatorError):
     """
     Raised when the verification failed (i.e. it's invalid).
     """
 
 
-class VerifyFacilitatorUnknownError(BaseError):
+class VerifyFacilitatorUnknownError(BaseFacilitatorError):
     """
     Raised when the verification had an error.
     """
 
 
-class SettleBadResponse(BaseError):
+class SettleBadResponse(BaseFacilitatorError):
     """
     Raised when the settling returned a non-2xx error.
     """
 
 
-class SettleFacilitatorFailedError(BaseError):
+class SettleFacilitatorFailedError(BaseFacilitatorError):
     """
     Raised when the settling had an error.
     """
 
 
-class SettleFacilitatorUnknownError(BaseError):
+class SettleFacilitatorUnknownError(BaseFacilitatorError):
     """
     Raised when the settling had an error.
     """
