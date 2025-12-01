bdb7081e0d3e9750d91b828e6c1d89476aa90707
Luis Masuelli <luisfmasuelli@gmail.com>
2025-11-30 21:12:04 -0500

    Finishing the httpx adapters for workers.

--- python/src/y402/workers/httpx_sync.py
+++ python/src/y402/workers/httpx_sync.py
@@ -1,8 +1,10 @@
 import asyncio
 import inspect
 import logging
+import threading
+import time
 from typing import Any, List
-from httpx import AsyncClient
+from httpx import Client
 from ..core.types.payment import SettledPayment
 from ..core.types.storage import StorageManager
 
@@ -11,13 +13,14 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
 
 
-async def _maybe_await(result: Any) -> Any:
+def _forbid_awaitable(result: Any, method: str) -> Any:
     if inspect.isawaitable(result):
-        result = await result
+        raise TypeError(f"The result of StorageManage.{method}(...) must not be an awaitable "
+                        "in this process_payment implementation")
     return result
 
 
-async def _send_batch(
+def _send_batch(
     worker_id: str, webhook_name: str, webhook_url: str, api_key: str,
     manager: StorageManager, collection: str, logger: logging.Logger
 ):
@@ -36,10 +39,10 @@ async def _send_batch(
         logger: An optional logger.
     """
 
-    async def _send(client: AsyncClient, payload: SettledPayment):
+    def _send(client: Client, payload: SettledPayment):
         try:
-            await client.post(url=webhook_url, headers=headers, json=payload.model_dump(mode="json"))
-            await _maybe_await(manager.mark_as_sent(collection, element.id_))
+            client.post(url=webhook_url, headers=headers, json=payload.model_dump(mode="json"))
+            _forbid_awaitable(manager.mark_as_sent(collection, element.id_), "mark_as_sent")
         except:
             logger.exception(f"An exception occurred when processing payment with id={payload.id_}")
 
@@ -47,48 +50,18 @@ async def _send_batch(
     headers = {"X-API-Key": api_key} if api_key else {}
     logger.info("Processing a batch:")
     logger.info("- Retrieving the batch...")
-    batch: List[SettledPayment] = await _maybe_await(manager.get_batch(collection, webhook_name, worker_id))
+    batch: List[SettledPayment] = _forbid_awaitable(manager.get_batch(collection, webhook_name, worker_id),
+                                                    "get_batch")
     logger.info("- Sending the batch...")
-    with AsyncClient(timeout=15) as client:
-        await asyncio.gather(*[_send(client, payload) for payload in batch])
+    with Client(timeout=15) as client:
+        threads = [threading.Thread(target=lambda: _send(client, payload)) for payload in batch]
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
     logger.info("- Updating the batch...")
     for element in batch:
         manager.mark_as_sent(collection, element.id_)
-
-
-async def _webhook_worker(
-    worker_id: str, webhook_name: str, webhook_url: str, api_key: str,
-    manager: StorageManager, collection: str, logger: logging.Logger = None,
-    sleep_time: int = 5
-):
-    """
-    Sends a batch of settled requests, assigned to a webhook name, to the
-    corresponding webhook URL (and with proper authentication, if any).
-    It uses a specific worker name for this, so it does not collide with
-    other workers in the same process.
-
-    Args:
-        worker_id: The id of this worker.
-        webhook_name: The name of the webhook to look records for.
-        webhook_url: The URL to send requests to.
-        api_key: The api key to use in the X-API-Key header, if any.
-        manager: The associated storage manager.
-        collection: The collection to use with the manager.
-        logger: An optional logger.
-        sleep_time: A sleep time between iterations.
-    """
-
-    logger = logger or LOGGER
-    
-    logger.info(f"Starting webhook worker loop.\n- webhook_name={webhook_name}\n- worker_id={worker_id}")
-    try:
-        while True:
-            await _send_batch(worker_id, webhook_name, webhook_url, api_key, manager, collection, logger)
-            await asyncio.sleep(sleep_time)
-    except KeyboardInterrupt:
-        logger.info(f"Worker loop cancelled by the user")
-    except:
-        logger.exception("Worker loop stopped because of an error:")
 
 
 def webhook_worker(
@@ -113,5 +86,14 @@ def webhook_worker(
         sleep_time: A sleep time between iterations.
     """
 
-    asyncio.run(_webhook_worker(worker_id, webhook_name, webhook_url, api_key, manager, collection,
-                                logger, sleep_time))
+    logger = logger or LOGGER
+
+    logger.info(f"Starting webhook worker loop.\n- webhook_name={webhook_name}\n- worker_id={worker_id}")
+    try:
+        while True:
+            _send_batch(worker_id, webhook_name, webhook_url, api_key, manager, collection, logger)
+            time.sleep(sleep_time)
+    except KeyboardInterrupt:
+        logger.info(f"Worker loop cancelled by the user")
+    except:
+        logger.exception("Worker loop stopped because of an error:")
