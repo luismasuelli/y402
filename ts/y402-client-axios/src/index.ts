722af965cc32f367bf7faa80e33361cf36808e42
Luis Masuelli <luisfmasuelli@gmail.com>
2026-01-25 19:32:51 -0500

    Adding more imports for the new requirements.

---
+++ ts/y402-client-axios/src/index.ts
@@ -0,0 +1,4 @@
+import {
+    PaymentRequirements, type TypedDataSigner, createSignedHeader, decodeHeader,
+    PaymentError, defaultPaymentRequirementsSelector, PaymentRequiredSelector
+} from "y402";
--- ts/y402-client-fetch/src/index.ts
+++ ts/y402-client-fetch/src/index.ts
@@ -1,46 +1,7 @@
-import { PaymentRequirements, type TypedDataSigner, createTypedDataSigner, createSignedHeader } from "y402";
-
-// Exports this signer, so it can be used by the customers.
-export { createTypedDataSigner };
-
-/**
- * The payment error.
- */
-export class PaymentError extends Error {
-    constructor(message: string) {
-        super(message);
-    }
-}
-
-/**
- * Decodes a JSON header.
- * @param header The header.
- */
-function decodeHeader(header: string): string {
-    const decoded = Buffer.from(header, "base64").toString("utf-8");
-    return JSON.parse(decoded);
-}
-
-/**
- * A default selector of payment requirements
- * @param requirements
- */
-function defaultPaymentRequirementsSelector(requirements: PaymentRequirements[]): PaymentRequirements {
-    for(let requirement of requirements) {
-        if (requirement.scheme === "exact") {
-            return requirement;
-        }
-    }
-    throw new PaymentError("No supported payment scheme found");
-}
-
-/**
- * A payment required selector, which can be an async function,
- * is used to choose a payment from a list of payments.
- */
-export type PaymentRequiredSelector =
-    ((requirements: PaymentRequirements[]) => PaymentRequirements) |
-    ((requirements: PaymentRequirements[]) => Promise<PaymentRequirements>);
+import {
+    PaymentRequirements, type TypedDataSigner, createSignedHeader, decodeHeader,
+    PaymentError, defaultPaymentRequirementsSelector, PaymentRequiredSelector
+} from "y402";
 
 /**
  * Creates a wrapped version of fetch, which uses x402 when
