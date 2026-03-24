4581b255a79e961b6049af1f8b5dfefbd746a1ac
Luis Masuelli <luisfmasuelli@gmail.com>
2026-03-24 11:36:16 -0500

    Fixing the client's exported names for Streamlit.

--- python/src/y402/clients/streamlit/__init__.py
+++ python/src/y402/clients/streamlit/__init__.py
@@ -2,17 +2,5 @@ from .common import (
     StreamlitPaymentFlowResult,
     StreamlitRequestResult,
     StreamlitWalletNotConnectedError,
-    StreamlitY402Client,
+    StreamlitY402Client
 )
-from .httpx_sync import Y402Client as Y402HttpxClient
-from .requests import Y402RequestsClient, y402_requests
-
-__all__ = [
-    "StreamlitPaymentFlowResult",
-    "StreamlitRequestResult",
-    "StreamlitWalletNotConnectedError",
-    "StreamlitY402Client",
-    "Y402HttpxClient",
-    "Y402RequestsClient",
-    "y402_requests",
-]
